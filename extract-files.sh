28ed156634c0947687588101fc62a761297e2dd4
Sultanxda <sultanxda@gmail.com>
2017-12-26 23:33:10 -0800

    dumpling: Fix fingerprint reader
    Change the fingerprint HAL name to "fingerprint.msm8998.so" since that's
    what libhardware expects, and automatically hex-edit the fingerprint HAL
    to change its ID from "gf_fingerprint" to "fingerprint" so that libhardware
    will load it.
    
    Also add the necessary sepolicy exceptions to make the fingerprint reader
    work. For some reason, these fingerprintd denials did not show up in logs,
    and had to be forced out using kernel hacks (by printing the audit messages
    using audit_printk_skb() instead of letting auditd print them).

--- extract-files.sh
+++ extract-files.sh
@@ -58,7 +58,16 @@ fi
 # Initialize the helper
 setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT" false "$CLEAN_VENDOR"
 
+# Remove incorrect fingerprint HAL from source so the extractor doesn't
+# overwrite our goodix fingerprint HAL with it instead of renaming our
+# goodix fingerprint HAL to fingerprint.msm8998.so
+rm -f "$SRC"/system/lib64/hw/fingerprint.msm8998.so
+
 extract "$MY_DIR"/proprietary-files-qc.txt "$SRC" "$SECTION"
 extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
 
 "$MY_DIR"/setup-makefiles.sh
+
+FINGERPRINT_HAL="$CM_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/lib64/hw/fingerprint.msm8998.so
+
+sed -i 's/\x00gf_fingerprint\x00/\x00fingerprint\x00\x00\x00\x00/' "$FINGERPRINT_HAL"
