fe6ede2ead028a506305c19595e7210aa89e7641
Sultanxda <sultanxda@gmail.com>
2017-12-26 23:25:48 -0800

    dumpling: Remove Qualcomm perf components
    These will be replaced with custom perf stuff in the kernel.

--- device.mk
+++ device.mk
@@ -207,10 +207,6 @@ PRODUCT_PACKAGES += \
     ebtables \
     ethertypes
 
-# IRQ
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/msm_irqbalance.conf:system/vendor/etc/msm_irqbalance.conf
-
 # IRSC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
