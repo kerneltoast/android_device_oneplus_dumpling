12f4057fefb142d34c967e14a6e88536c1585a31
Jason A. Donenfeld <Jason@zx2c4.com>
2018-01-05 04:19:49 +0100

    dumpling: we are the OnePlus 5T, not 5

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -23,7 +23,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME "OnePlus 5"
+#define BTM_DEF_LOCAL_NAME "OnePlus 5T"
 #define BLUETOOTH_QTI_SW TRUE
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
